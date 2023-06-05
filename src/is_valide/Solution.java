74ce22a2db579d9e62b894287745708ee06ab848
laowang <1390628669@qq.com>
2023-06-05 23:17:44 +0800

    20题

--- src/is_valide/Solution.java
+++ src/is_valide/Solution.java
@@ -30,50 +30,18 @@ public class Solution {
         if (s.length() % 2 != 0) {
             return false;
         }
-        char[] charArray = s.toCharArray();
-        //判断第一个如果是{，则第二个必须是}，以此类推
-        for (int i = 0; i < charArray.length - 1; i += 2) {
-            //从0开始为第一
-            if (charArray[i] == 91 && charArray[i + 1] == 93) {
-                return true;
-            }
-            if (charArray[i] == 123 && charArray[i + 1] == 125) {
-                return true;
-            }
-            if (charArray[i] == 40 && charArray[i + 1] == 41) {
-                return true;
-            }
-        }
-        return false;
-    }
 
-    public boolean isValid2(String s) {
-        //首先肯定必须是偶数
-        if (s.length() % 2 != 0) {
-            return false;
+        int length = s.length() / 2;
+        for (int i = 0; i < length; i++) {
+            s = s.replace("()", "").replace("{}", "").replace("[]", "");
         }
-        char[] charArray = s.toCharArray();
-        Map<Integer, Integer> countMap = new HashMap<>();
-        //判断每个的数量是否相等
-        for (int i = 0; i < charArray.length; i++) {
-            //从0开始为第一
-            countMap.put((int) charArray[i], countMap.getOrDefault((int) charArray[i], 0) + 1);
-        }
-        if (countMap.containsKey(91) && countMap.containsKey(93) && Objects.equals(countMap.get(91), countMap.get(93))) {
-            return true;
-        }
-        if (countMap.containsKey(123) && countMap.containsKey(125) && Objects.equals(countMap.get(123), countMap.get(125))) {
-            return true;
-        }
-        if (countMap.containsKey(40) && countMap.containsKey(41) && Objects.equals(countMap.get(40), countMap.get(41))) {
-            return true;
-        }
-        return false;
+
+        return s.length() == 0;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        boolean valid = solution.isValid2("([)]");
+        boolean valid = solution.isValid("([)]");
         System.out.println(valid);
     }
 }
